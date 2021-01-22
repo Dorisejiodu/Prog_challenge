3eec6bd0151c0663474f32edd036fd9b273dfb39
Doris Ejiodu <doris.ejiodu@ingka.com>
2021-01-22 18:43:05 +0100

    new code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,3 +4,13 @@
 # If it is divisible by both 3 and 5, it should return “FizzBuzz”.
 # Otherwise, it should return the same number.
 
+
+int1 = int(input("Enter a number "))
+if ((int1 % 3 == 0) & (int1 % 5 != 0)):
+    print("Fizz")
+if (int1 % 5 == 0) & (int1 % 3 != 0):
+    print("Buzz")
+if ((int1 % 3== 0) & (int1 % 5 == 0)):
+    print("FizzBuzz")
+if ((int1 % 3 != 0) & (int1 %5 != 0)):
+    print(int1)
