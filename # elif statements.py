7cae6dc5d2774a628d474c712db8e848941ad497
terry ilebode <terryilebode6@yahoo.com>
2021-01-24 13:25:08 +0100

    Review comments

--- # elif statements.py
+++
@@ -1,8 +0,0 @@
-# elif statements
-x = float(input("Enter a number  "))
-if x < 2:
-    print("The number is less than 2.")
-elif x < 6:
-    print("The number is less than 6.")
-elif x < 15:
-    print("The number is less than 15.")
